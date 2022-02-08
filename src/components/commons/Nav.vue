290126e4ef40aab92b5a13faef6e2e4004202883
Osnago-L <osna.brera@gmail.com>
2022-02-08 13:10:47 +0100

    header end

---
+++ src/components/commons/Nav.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="nav-header container">
+    <img class="logo" src="../../assets/img/avada-bakery-logo-retina-200x97.png" alt="">
+    <nav>
+        <span v-for="element,index in 8" :key="index"
+        >test</span>
+
+        <img class="cart" src="../../assets/img/cart-shopping-solid.svg" alt="">
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Nav",
+    data(){
+      return{
+
+      }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/style/partial/variables.scss";
+.nav-header{
+  height: 100px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  .logo{
+  width: 150px;
+  }
+  nav{
+    
+    span{
+      margin: 0 30px;
+      padding-bottom: 8px;
+      text-transform: uppercase;
+      font-weight: 500;
+      font-size: 12px;
+      color: $daisy-bush;
+      opacity: 0.8;
+
+      &:hover{
+        font-weight: 700;
+        border-bottom: 2px solid $daisy-bush;
+        cursor: pointer;
+      }
+    }
+    .cart{
+      width: 12px;
+      margin: 0 30px;
+
+      &:hover{
+        cursor: pointer;
+      }
+    }
+  }
+}
+
+</style>
--- src/components/macro/Header.vue
+++ src/components/macro/Header.vue
@@ -1,15 +1,72 @@
 <template>
   <div class="header">
-
+    <Nav/>
+    <div class=" jumbo container">
+      <div class="jumbo-text">
+        <span>Fresh & Tasty Bakery Every Day</span>
+        <h1 class="title">The Perfect Fresh Bread</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis nemo similique soluta officiis.</p>
+        <button>Explore our products</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Nav from "../commons/Nav.vue";
+
 export default {
-    name: "Header"
+    name: "Header",
+    components:{
+      Nav
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+@import "../../assets/style/partial/variables.scss";
+
+.header{
+  width: 100%;
+  height: 700px;
+  max-height: 700px;
+  background-image: url("../../assets/img/hero-header-bakery.jpg");
+  background-repeat: repeat;
+  background-position: bottom ;
+  background-size: cover;
+}
+.jumbo{
+  height: 100%;
+  position: relative;
+  top: 20%;
+
+  .jumbo-text{
+  width: 500px;
+
+  &>span{
+    font-size: 14px;
+    color: #929295;
+  }
+  &>.title{
+    font-size: 50px;
+    color: $daisy-bush;
+    margin: 10px 0;
+  }
+  &>p{
+    font-size: 17px;
+    color: #929295;
+    margin-bottom: 20px;
+  }
+  &>button{
+    border: none;
+    border-radius: 6px 6px;
+    background-color: white;
+    padding: 10px 30px;
+    font-size: 14px;
+    color: $daisy-bush;
+    cursor: pointer;
+  }
+  }
+}
 
 </style>
